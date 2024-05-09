26f3e3074cbce4e9d30c0ff7cea1adedb7328724
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-09 21:53:56 +0000

    May 9, 2024, 9:53 PM

--- Week01/main.py
+++ Week01/main.py
@@ -4,3 +4,14 @@ id(test)
 # None = 'COMP216'
 type(None)
 dir(__builtins__)
+type(0b1)
+print(0o10)
+print(0b10)
+type(0b10)
+num = 100_000_000
+num
+d_num = 1.7976e308
+d_num_1 = 1.7977e308
+d_num_1
+d_num_2 = -1.7977e308
+d_num_2
