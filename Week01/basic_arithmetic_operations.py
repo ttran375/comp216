fb0f8e5d37ff8f64585f4856536bd9a8d0b5908c
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-09 20:56:42 +0000

    May 9, 2024, 8:56 PM

---
+++ Week01/basic_arithmetic_operations.py
@@ -0,0 +1,9 @@
+# Basic arithmetic operations
+result1 = 4 + 5
+result2 = 89 - 4.3
+a = 3 * 4
+a = 12
+print(f"value of a is {a}")
+print("Type of a:", type(a))
+print("Attributes of a:", dir(a))
+print("Help on a:", help(a))
--- Week01/main.py
+++ Week01/main.py
@@ -1,12 +1,4 @@
-# Basic arithmetic operations
-result1 = 4 + 5
-result2 = 89 - 4.3
-a = 3 * 4
-a = 12
-print(f"value of a is {a}")
-print("Type of a:", type(a))
-print("Attributes of a:", dir(a))
-print("Help on a:", help(a))
+
 
 # Type conversion
 int_num = int(3.14)
