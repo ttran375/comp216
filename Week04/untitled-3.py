3ec75117c66f227b265c46d05d55ed481f81c0d7
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-30 21:09:51 +0000

    Thu, May 30, 2024, 9:09 PM +00:00

--- Week04/untitled-3.py
+++ Week04/untitled-3.py
@@ -5,12 +5,15 @@ def f():
     x = "variable_2"  # Enclosing
 
     def g():
-        global x
-        print(x)  # Local
+        # global x
+        # print(x)  # Local
         # print(locals())
 
         # print(global x)  # Local
         # print(locals())
+
+        # nonlocal x
+        print(x)
 
     g()
     # print(locals())
