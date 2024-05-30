150431fe9b4232bf65821d2f64d676d45180ae07
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-30 21:50:12 +0000

    Thu, May 30, 2024, 9:50 PM +00:00

--- Week04/untitled-6.py
+++ Week04/untitled-6.py
@@ -1,16 +1,19 @@
-import calc as c
+import importlib
+# import calc as c
+import calc
 
 print(globals)
 
-output_1 = c.add(4, 7)
-print(output_1)
+# output_1 = c.add(4, 7)
+# print(output_1)
 
-output_2 = c.diff(2, 9)
-print(output_2)
+# output_2 = c.diff(2, 9)
+# print(output_2)
 
 print(globals)
 
-import calc
-import calc
+importlib.reload(calc)
+# import calc
+# import calc
 
 print(globals())
