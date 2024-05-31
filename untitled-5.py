20c2ba15e09fce98908227f80a3c90493c8826f4
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-31 21:59:17 +0000

    Fri, May 31, 2024, 9:59 PM +00:00

--- untitled-5.py
+++ untitled-5.py
@@ -8,4 +8,13 @@ add_2 = lambda x, y, z: x + y + z + 2
 print(add_2)
 print(add_2(3, 5, 8))
 
-full_name = lambda first_name, last_name: first_name + " " + last_name
+full_name = lambda fn, ln: f"Full Name of User: {fn.upper()} {ln.upper()}"
+field_1 = "Bob"
+field_2 = "Wells"
+
+print(full_name(field_1, field_2))
+
+
+basic_fn = lambda func, a: a + func(a)
+nest_lambda = basic_fn(lambda a: a**3, 3)
+print(nest_lambda)
