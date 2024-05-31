31132a26cbf0baa31f39704f0ac603a616f9b877
ttran375 <ttran375@my.centennialcollege.ca>
2024-05-31 21:53:35 +0000

    Fri, May 31, 2024, 9:53 PM +00:00

--- untitled-5.py
+++ untitled-5.py
@@ -2,7 +2,10 @@
 a = (lambda x: x + 2)(2)
 print(a)
 
-add_2 = lambda x: x + 2
+# add_2 = lambda x: x + 2
+add_2 = lambda x, y, z: x + y + z + 2
 
 print(add_2)
-print(add_2(3))
+print(add_2(3, 5, 8))
+
+full_name = lambda first_name, last_name: first_name + " " + last_name
