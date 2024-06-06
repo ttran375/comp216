9c71f949e3d28db40b65812f81b67810f50d5527
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-06 20:49:33 +0000

    Thu, Jun 6, 2024, 8:49 PM +00:00

--- install.sh
+++ install.sh
@@ -1,16 +1,7 @@
-#!/bin/bash
-
-# Update package list
 sudo apt-get update
-
-# Install Wireshark
 sudo apt-get install -y wireshark
-
-# Allow non-root users to capture packets
 sudo dpkg-reconfigure wireshark-common
-
-# Add current user to the Wireshark group
 sudo usermod -aG wireshark $USER
-
-# Verify installation
 wireshark --version
+sudo apt-get install -y tshark
+sudo tshark -i eth0
