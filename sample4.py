d0f61b232d4c40a17a909f418af72ca2edcb2ac9
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 21:44:29 +0000

    Fri, Jun 14, 2024, 9:44 PM +00:00

--- sample4.py
+++ sample4.py
@@ -14,3 +14,11 @@ except Exception as err:
 else:
     header = response.headers
     print(type(header))
+
+    print(header)
+
+    date = header["Date"]
+    print(date)
+    cont_type_1 = header["Content-Type"]
+    cont_type_2 = header["content-type"]
+    print(cont_type_1, cont_type_2)
