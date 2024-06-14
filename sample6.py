a477b61cf8eeee5c1a3da45ab2fe3015e27f65f7
T. Tran <ttran375@my.centennialcollege.ca>
2024-06-14 22:27:34 +0000

    Fri, Jun 14, 2024, 10:27 PM +00:00

--- sample6.py
+++ sample6.py
@@ -4,7 +4,17 @@ import requests
 url = "https://httpbin.org/post"
 input = {"k1": "v1", "k2": "v2", "k3": "v3"}
 
-response = requests.post(url, data=input)
+filename = "test.txt"
+
+upl_file = open(filename, "rb")
+
+# response = requests.post(url, data=input)
+# response = requests.post(url, json=input)
+# response = requests.post(upl_file, files={"file:": upl_file})
+
+response = requests.post(url, files={"file": upl_file})
+    
+
 print(response)
 print("\n")
 print(response.headers)
