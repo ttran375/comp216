33df6cbc4142fd1800e585218210d7eea1fd8256
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-20 21:17:53 +0000

    Thu, Jun 20, 2024, 9:17 PM +00:00

--- sample2.py
+++ sample2.py
@@ -1,6 +1,9 @@
 import requests
 
 url = "https://httpbin.org/cookies"
+
+session = requests.Session()
+
 ns_response = requests.get(url)
 print(ns_response.text)
 
@@ -9,3 +12,9 @@ print(ns_response_1.text)
 
 ns_response_2 = requests.get(url + "/set/input2/value2")
 print(ns_response_2.text)
+
+s_response_1 = session.get(url + "/set/input1/value1")
+print(s_response_1.text)
+
+s_response_2 = session.get(url + "/set/input2/value2")
+print(s_response_2.text)
