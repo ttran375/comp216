3b646d22f5155f31c9d9f1692e6cf9dfe9589cd1
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-20 20:52:19 +0000

    Thu, Jun 20, 2024, 8:52 PM +00:00

--- sample1.py
+++ sample1.py
@@ -2,9 +2,11 @@ import requests
 
 url = "https://httpbin.org/post"
 
-input = {"k1": "v1", "k2": "v2", "k3": "v3"}
+# input = {"k1": "v1", "k2": "v2", "k3": "v3"}
+input = {"k10": "v10", "k20": "v20", "k30": "v30"}
 
-response = requests.post(url, data=input)
+# response = requests.post(url, data=input)
+response = requests.post(url, json=input)
 
 print(response.headers)
 print(response.json())
