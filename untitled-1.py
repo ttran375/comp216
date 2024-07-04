1166f4e149cf918871fe681b20145b2d839e02a9
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-04 22:00:37 +0000

    Thu, Jul 4, 2024, 10:00 PM +00:00

--- untitled-1.py
+++ untitled-1.py
@@ -1,1 +1,21 @@
 import smtplib
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+SERVER = "smtp.gmail.com"
+PORT = 587
+EMAIL_ACC = "cen.comp216@gmail.com"
+PASSWORD = os.getenv("PASSWORD")
+
+RECPT = "cen.comp216@gmail.com"
+
+message = f"""\
+From: {EMAIL_ACC}
+To: {RECPT}
+Subject: SMTP Test
+
+Testing new Python app. First email!
+"""
