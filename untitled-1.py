5a558655134c3e4c4911effe2eeb290096a3c742
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-04 22:18:06 +0000

    Thu, Jul 4, 2024, 10:18 PM +00:00

--- untitled-1.py
+++ untitled-1.py
@@ -1,6 +1,7 @@
 import smtplib
 from dotenv import load_dotenv
 import os
+from email.mime.text import MIMEText
 
 load_dotenv()
 
@@ -11,6 +12,13 @@ EMAIL_ACC = "cen.comp216@gmail.com"
 PASSWORD = os.getenv("PASSWORD")
 
 RECPT = "cen.comp216@gmail.com"
+
+html_m = "<h1><body><b>Hello</b>--COMP216</body></h1>"
+
+email_content = MIMEText(html_m, "html")
+email_content["From"] = EMAIL_ACC
+email_content["To"] = RECPT
+email_content["Subject"] = "COMP216 HTML Email"
 
 message = f"""\
 From: {EMAIL_ACC}
@@ -24,7 +32,7 @@ try:
     with smtplib.SMTP(SERVER, PORT) as smtp:
         smtp.starttls()
         smtp.login(EMAIL_ACC, PASSWORD)
-        smtp.sendmail(EMAIL_ACC, RECPT, message)
+        smtp.sendmail(EMAIL_ACC, RECPT, email_content.as_string())
 except Exception as e:
     print(f"Error: {e}")
 else:
