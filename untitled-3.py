2a4981ae6a4de7999ca3df774b3b341c301ecb90
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-05 21:07:08 +0000

    Fri, Jul 5, 2024, 9:07 PM +00:00

--- untitled-3.py
+++ untitled-3.py
@@ -2,6 +2,8 @@ import smtplib
 from dotenv import load_dotenv
 import os
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 load_dotenv()
 
@@ -14,23 +16,24 @@ PASSWORD = os.getenv("PASSWORD")
 RECPT = "cen.comp216@gmail.com"
 
 html_m = "<html><body><b>Hello</b><br>Testing HTML Email</br>--COMP216</body></html>"
-
-
-email_content = MIMEText(html_m, "html")
+h_email_content = MIMEText(html_m, "html")
 
 txt_m = "Hello Testing HTML Email --COMP216"
-t_email_content = MIMEText(txt_m, "text")
+t_email_content = MIMEText(txt_m, "plain")
 
-'''email_content["From"] = EMAIL_ACC
-email_content["To"] = RECPT
-email_content["Subject"] = "Testing HTML Email Message"'''
+email_msg = MIMEMultipart()
+email_msg["From"] = EMAIL_ACC
+email_msg["To"] = RECPT
+email_msg["Subject"] = "Testing HTML Email Message V2"
 
+email_msg.attach(h_email_content)
+email_msg.attach(t_email_content)
 
 try:
     with smtplib.SMTP(SERVER, PORT) as smtp:
         smtp.starttls()
         smtp.login(EMAIL_ACC, PASSWORD)
-        smtp.sendmail(EMAIL_ACC, RECPT, email_content.as_string())
+        smtp.sendmail(EMAIL_ACC, RECPT, email_msg.as_string())
 except Exception as e:
     print(f"Error: {e}")
 else:
