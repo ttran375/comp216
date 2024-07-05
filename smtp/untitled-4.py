2802781ee3125c37be424dc3fe8bd154dd6d9af8
T. Tran <ttran375@my.centennialcollege.ca>
2024-07-05 21:23:53 +0000

    Fri, Jul 5, 2024, 9:23 PM +00:00

--- smtp/untitled-4.py
+++ smtp/untitled-4.py
@@ -7,7 +7,6 @@ from email.mime.text import MIMEText
 
 from email.mime.multipart import MIMEMultipart
 
-
 load_dotenv()
 
 
@@ -33,13 +32,15 @@ with open(filename, "rb") as upload_file:
     attachment.set_payload(upload_file.read())
 
 encoders.encode_base64(attachment)
-attachment.add_header('Content-Disposition', f'attachment; filename={filename}')
+attachment.add_header("Content-Disposition", f"attachment; filename={filename}")
 
 
 email_msg = MIMEMultipart()
 email_msg["From"] = EMAIL_ACC
 email_msg["To"] = RECPT
-email_msg["Subject"] = "Testing HTML Email Message V2"
+email_msg["Subject"] = "Email with attachment V3"
+email_msg.attach(attachment)
+
 
 email_msg.attach(h_email_content)
 email_msg.attach(t_email_content)
