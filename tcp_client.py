4b85b3124a3b1f7164bb4a29e015727d8b3b3645
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-08 21:44:15 +0000

    Thu, Aug 8, 2024, 9:44 PM +00:00

--- tcp_client.py
+++ tcp_client.py
@@ -4,5 +4,6 @@ SERVER_HOST = "localhost"
 SERVER_PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
-    tcp_client.connect(SERVER_HOST, SERVER_PORT)
-    print(f"Client connect to the server")
+    print(f"Socket object: {tcp_client}")
+    tcp_client.connect((SERVER_HOST, SERVER_PORT))
+    print(f"Client connected to the server")
--- tcp_server.py
+++ tcp_server.py
@@ -10,3 +10,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
     client_conn, addr = tcp_server.accept()
     print(f"Client connected from IP: {addr[0]} via Port: {addr[1]}")
     print(client_conn)
+
+    with client_conn:
+        while True:
+            data = client_conn.recv(1024)
+            if not data:
+                break
+            print(f"Received data: {data.decode()}")
+            client_conn.sendall(data)
+            print(f"Sent data: {data.decode()}")
