c47a3213bd760811885805ca2177c04e25669eb6
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-08 21:40:38 +0000

    Thu, Aug 8, 2024, 9:40 PM +00:00

--- tcp_client.py
+++ tcp_client.py
@@ -1,1 +1,8 @@
 import socket
+
+SERVER_HOST = "localhost"
+SERVER_PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
+    tcp_client.connect(SERVER_HOST, SERVER_PORT)
+    print(f"Client connect to the server")
