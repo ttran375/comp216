532e3c23a43e415e15dc7cd720fbdfc8321ba9e6
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-08 21:53:47 +0000

    Thu, Aug 8, 2024, 9:53 PM +00:00

--- tcp_client.py
+++ tcp_client.py
@@ -1,9 +1,16 @@
 import socket
+import time
 
 SERVER_HOST = "localhost"
 SERVER_PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
-    print(f"Socket object: {tcp_client}")
     tcp_client.connect((SERVER_HOST, SERVER_PORT))
     print(f"Client connected to the server")
+
+    for i in range(4):
+        outbound_message = f"Message {i}"
+        tcp_client.sendall(outbound_message.encode())
+        inbound_message = tcp_client.recv(1024)
+        print(f'Received from server: {inbound_message.decode("utf-8")}')
+        time.sleep(1)
--- tcp_server.py
+++ tcp_server.py
@@ -9,13 +9,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
     tcp_server.listen()
     client_conn, addr = tcp_server.accept()
     print(f"Client connected from IP: {addr[0]} via Port: {addr[1]}")
-    print(client_conn)
+    # print(client_conn)
 
     with client_conn:
         while True:
-            data = client_conn.recv(1024)
-            if not data:
+            inbound_message = client_conn.recv(1024)
+            if not inbound_message:
                 break
-            print(f"Received data: {data.decode()}")
-            client_conn.sendall(data)
-            print(f"Sent data: {data.decode()}")
+            print(f"Received from client: {inbound_message.decode('utf-8')}")
+            outboud_msg = "Server processed message:" + inbound_message.decode("utf-8")
+            client_conn.send(outboud_msg.encode())
+            print(f"Sent data: {inbound_message.decode()}")
