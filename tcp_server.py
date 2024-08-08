e24949601b440e54fd2f003da8e49903212dacb6
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-08 21:33:29 +0000

    Thu, Aug 8, 2024, 9:33 PM +00:00

---
+++ tcp_server.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = "localhost"
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
+    tcp_server.bind((HOST, PORT))
+    print("Server is running.")
+    tcp_server.listen()
+    client_conn, addr = tcp_server.accept()
+    print(f"Client connected from IP: {addr[0]} via Port: {addr[1]}")
+    print(client_conn)
