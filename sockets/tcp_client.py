1ed2cbc01eba8f8b93c551f1efa6d71b4f265ff2
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-09 21:26:47 +0000

    Fri, Aug 9, 2024, 9:26 PM +00:00

--- sockets/tcp_client.py
+++ sockets/tcp_client.py
@@ -4,6 +4,19 @@ import time
 SERVER_HOST = "localhost"
 SERVER_PORT = 12345
 
+
+def incoming_message(client_soc_obj):
+    while True:
+        incoming_message = client_soc_obj.recv(2048)
+        print(incoming_message.decode("utf-8"))
+
+
+def sending_message(client_soc_obj):
+    while True:
+        outbound_message = input("Enter message: ")
+        client_soc_obj.sendall(outbound_message.encode())
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
     tcp_client.connect((SERVER_HOST, SERVER_PORT))
     print("Client connected to the server")
