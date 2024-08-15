1795487d9a362eb3f97a49eb1b435ad3cece0be4
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-15 20:53:28 +0000

    Update tcp server I

--- sockets/tcp_server.py
+++ sockets/tcp_server.py
@@ -7,9 +7,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_server:
     tcp_server.bind((HOST, PORT))
     tcp_server.listen()
     client_conn, addr = tcp_server.accept()
+    print(f"Client conneted from IP: {addr[0]} via Port: {addr[1]}")
 
     with client_conn:
-        print(f"Client conneted from IP: {addr[0]} via Port: {addr[1]}")
         while True:
             data = client_conn.recv(1024)
             if not data:
